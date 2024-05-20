cfd50c47a41d1237d5adc66917a874de9b790b58
nhatduong146 <nhatduong146@gmail.com>
2024-05-20 23:10:25 +0700

    init source code

---
+++ AZ900AzureFunctionDemo/HttpTriggerFunction.cs
@@ -0,0 +1,30 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace AZ900AzureFunctionDemo
+{
+    public class HttpTriggerFunction
+    {
+        private readonly ILogger _logger;
+
+        public HttpTriggerFunction(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<HttpTriggerFunction>();
+        }
+
+        [Function("HttpTriggerFunction")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Welcome to Azure Functions! Andrew");
+
+            return response;
+        }
+    }
+}
